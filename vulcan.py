dc8d9c7366f0f8fe53ee40171f8ee37033c53425
Zahi <2711358695@qq.com>
2021-08-22 19:37:21 +0800

    add feature vulcan

---
+++ vulcan.py
@@ -0,0 +1,43 @@
+#This is our very first meanwhile signifcant procedure for our whole grand plan which aims at unmannedly advancing mankind's endless permanent desire for the vast while splandid universe. And I'll take this very first step to spare no effort or expense to achieve this, actually this is why this porogram coming into existence.    -----Wrote in 2050, Zahi Rob.
+
+import time
+
+class Vulcan():
+    def __init__():
+        self.getComponets()
+        self.getEngineer()
+        self.robInvestment()
+        self.purchaseOil()
+        self.makeIt()
+        self.addOil(£©
+        self.peopleAbord()
+        self.speedUp()
+
+    def getComponents():
+        self.compnents = "Aided by NASA"
+
+    def getEngineer():
+        self.engineer = "Salary least upon 1 billion$ per month"
+
+    def robInvestment():
+        self.money = "Purchase a cheap but useful CS helmet."
+
+    def purchaseOil():
+        self.oil = self.money / 1.9
+
+    def makeIt():
+        engineer_hair = None
+
+    def addOil():
+        self.energy = self.oil
+
+    def peopleAbord():
+        self.crew = "100 billion$ per"
+
+    def speedUp():
+        self.speed = 0
+        while speed <= light_speed:
+            speed += 10
+            time.sleep(0.0000000000000000000000000001)
+
+print("WE MAKE IT!!!!")
